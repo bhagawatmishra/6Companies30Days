30c10cdc0ea2472b73fdfe756adc4b38e1788dd7
Bhagawat Mishra <bhagawat.cse.1812@iiitbh.ac.in>
2022-01-01 17:12:15 +0530

    Goldman Sachs

--- GS_Q-4.cpp
+++ GS_Q-4.cpp
@@ -1,9 +1,15 @@
-string encode(string src) {
+// https://practice.geeksforgeeks.org/problems/run-length-encoding/1/#
+// TC: O(n)  SC: O(1)
+
+string encode(string src)
+{
     string res;
-    for(int i = 0; i < src.size(); i++) {
+    for (int i = 0; i < src.size(); i++)
+    {
         res += src[i];
         int cnt = 1;
-        while(i+1 < src.size() and src[i] == src[i+1]) {
+        while (i + 1 < src.size() and src[i] == src[i + 1])
+        {
             cnt++;
             i++;
         }
