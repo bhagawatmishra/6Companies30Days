178e62b857f8e476c7c0828b69a65274ef58c836
Bhagawat Mishra <bhagawat.cse.1812@iiitbh.ac.in>
2022-01-01 16:29:44 +0530

    Goldman Sachs

---
+++ GS_Q-3.cpp
@@ -0,0 +1,33 @@
+// https://practice.geeksforgeeks.org/problems/count-the-subarrays-having-product-less-than-k1708/1/#
+// Sliding Window Technique
+// TC: O(N)    SC: O(1)
+
+class Solution
+{
+public:
+    int countSubArrayProductLessThanK(const vector<int> &a, int n, long long k)
+    {
+        if (k <= 1)
+            return 0;
+
+        long long int prod = 1;
+        int result = 0;
+
+        int left = 0;
+        int right = 0;
+
+        while (right < n)
+        {
+            prod *= a[right];
+            while (prod >= k)
+            {
+                prod /= a[left];
+                left++;
+            }
+            result += right - left + 1;
+            right++;
+        }
+
+        return result;
+    }
+};
